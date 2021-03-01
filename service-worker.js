032fff72dd88d956d785e72a7308d6bbff507eb5
Edison wang <x2512489632@gmail.com>
2021-03-01 18:35:54 +0800

    update litepaper dir

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.3a1c1df6cfcbefcce49c0bb0d9a4fd7d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.0861472a5782e59a797a10255c6015d1.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
