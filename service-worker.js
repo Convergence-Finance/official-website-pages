7cd197b010af4c0a620ddaa6a8d13ba26959bd6f
Edison wang <x2512489632@gmail.com>
2021-03-02 20:26:39 +0800

    add 404.html

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.faeb3066248b88daec17693565d983f0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.3a893d7358ad98483871646c4d7f2b0b.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
