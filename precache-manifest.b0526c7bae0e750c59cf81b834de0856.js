d5ce7ac86000cde5aa6b26ecbebd97c841ff4855
Edison wang <x2512489632@gmail.com>
2021-03-02 20:38:17 +0800

    update mail subscript input color

---
+++ precache-manifest.b0526c7bae0e750c59cf81b834de0856.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = [
+  {
+    "revision": "54749a5adbc8761267a12d9db0f4726e",
+    "url": "/img/clock-regular.54749a5a.svg"
+  },
+  {
+    "revision": "46abbc4a676739dbd61f8a305cb63fd8",
+    "url": "/img/nucleo-icons.46abbc4a.svg"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "76e08789ed2203bee1e5b270e4ed001c",
+    "url": "/img/Social-Network-Icons.76e08789.svg"
+  },
+  {
+    "revision": "acf3dcb7ff752b5296ca23ba2c7c2606",
+    "url": "/img/fontawesome-webfont.acf3dcb7.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "248b9d625f793717be5b69e4971f8b3e",
+    "url": "/img/sync-solid.248b9d62.svg"
+  },
+  {
+    "revision": "18756749360690cb2eb08bb9a9a4f5fc",
+    "url": "/exchange-alt-solid.svg"
+  },
+  {
+    "revision": "f1dd5f39002dacee23ab816b8620d087",
+    "url": "/copy-solid.svg"
+  },
+  {
+    "revision": "efcaa6d1f14ec3072e10c50976132520",
+    "url": "/exclamation-circle-solid.svg"
+  },
+  {
+    "revision": "8e56ec4e7c50d53aec9d0081eac8887e",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "8e56ec4e7c50d53aec9d0081eac8887e",
+    "url": "/favicon-96x96.png"
+  },
+  {
+    "revision": "8e56ec4e7c50d53aec9d0081eac8887e",
+    "url": "/favicon-48x48.png"
+  },
+  {
+    "revision": "8e56ec4e7c50d53aec9d0081eac8887e",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "/img/brand/blue.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "8e56ec4e7c50d53aec9d0081eac8887e",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "7204959c5a155452b3056a5018ebc1e9",
+    "url": "/img/brand/logo-decentral-invert@1.5x.png"
+  },
+  {
+    "revision": "e44ee97f8cd246b7472bbed866e73ea0",
+    "url": "/img/brand/logo-decentral@1.5x.png"
+  },
+  {
+    "revision": "19a053493fbe8a8bf5bc94050d452b9f",
+    "url": "/img/brand/pat-spots-001.png"
+  },
+  {
+    "revision": "e5265ff08976a766afbb5a6550fe4195",
+    "url": "/img/brand/pat-spots-002.png"
+  },
+  {
+    "revision": "244b4b5a46b26a9c2f0ba0d3aa768a71",
+    "url": "/img/brand/pat-spots-005.png"
+  },
+  {
+    "revision": "d7177319e37f6981dd7328357b576b6f",
+    "url": "/img/brand/pat-spots-004.png"
+  },
+  {
+    "revision": "4f117f10f7a7dad8a60857eb61cc3f56",
+    "url": "/img/brand/pat-spots-003.png"
+  },
+  {
+    "revision": "c21a63fc1a32fd4fb7db50ceb8c89a81",
+    "url": "/img/brand/icon-decentral@1.5x.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "/img/brand/white.png"
+  },
+  {
+    "revision": "b59857d4f72d8eda7f21e96026d39762",
+    "url": "/img/brand/icon-decentral-invert@1.5x.png"
+  },
+  {
+    "revision": "c411545789bec876bbf9bba670c4a53f",
+    "url": "/img/homepage/img-defi-ecosystem.png"
+  },
+  {
+    "revision": "b1e83852dab7eeee2d6523dc374dee3c",
+    "url": "/img/homepage/bg-abstract-spirally-expanding-dust.jpg"
+  },
+  {
+    "revision": "e587d1dc47a36762cfbe745d212067bb",
+    "url": "/img/homepage/img-defi-opportunity-graph.png"
+  },
+  {
+    "revision": "86d0fb10768f9ef5b88e388d009fe137",
+    "url": "/img/homepage/Graphic_4_Dark@3x.png"
+  },
+  {
+    "revision": "cd9a8415e97045704a473199c29367b6",
+    "url": "/img/homepage/img-defi-opportunity.jpg"
+  },
+  {
+    "revision": "58c76a8a28d1711e748ff30e31688a43",
+    "url": "/img/homepage/shot-commodities.jpg"
+  },
+  {
+    "revision": "bb2d053dc86fa065e51545d2364dde83",
+    "url": "/img/homepage/shot-trust-fund.jpg"
+  },
+  {
+    "revision": "a702488798b51800a631b1a937f6885a",
+    "url": "/img/homepage/shot-private-company.jpg"
+  },
+  {
+    "revision": "54749a5adbc8761267a12d9db0f4726e",
+    "url": "/clock-regular.svg"
+  },
+  {
+    "revision": "b4bb588183f8ef2612da3feb1db4b8c5",
+    "url": "/img/investers/1x/CMS Copy@1x.png"
+  },
+  {
+    "revision": "080d4c9ede5f57da616ed21c6640e9a6",
+    "url": "/img/investers/1x/Alameda Research Copy@1x.png"
+  },
+  {
+    "revision": "d80e0cea9e3cb616c71d0e1f3cca26cf",
+    "url": "/img/homepage/img-amm-solution.jpg"
+  },
+  {
+    "revision": "6fd27d6a4abc191c3a3f436a71784419",
+    "url": "/img/investers/1x/DuckDAO Copy@1x.png"
+  },
+  {
+    "revision": "f2e8b9c91f2070616857857bc8f665b2",
+    "url": "/img/investers/1x/GBV Copy@1x.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "9709d85df107ad0442ea7e2a7f4b0bcd",
+    "url": "/img/investers/1x/Hashed Copy@1x.png"
+  },
+  {
+    "revision": "139aae535f471fd48762ec1394462229",
+    "url": "/img/investers/1x/Individual 1 Copy@1x.png"
+  },
+  {
+    "revision": "b1396cf881a5d0989e0a16e03bb69bd4",
+    "url": "/img/investers/1x/Divergence Ventures Copy@1x.png"
+  },
+  {
+    "revision": "f2b8de5ab57de5105856fdd5992647bf",
+    "url": "/img/investers/1x/HashGlobal Copy@1x.png"
+  },
+  {
+    "revision": "77983865da02c9e8511c1df5496730d3",
+    "url": "/img/investers/1x/NGC Copy@1x.png"
+  },
+  {
+    "revision": "8a60aeb5ff5e4c8dd25d55b34296812a",
+    "url": "/img/investers/1x/Individual 2 Copy@1x.png"
+  },
+  {
+    "revision": "3f52a2e7277c92157c3dfa4a3a18d625",
+    "url": "/img/investers/1x/Kenetic Copy@1x.png"
+  },
+  {
+    "revision": "1f4b5fb67e67c6f4e8073b9c9efc67aa",
+    "url": "/img/investers/1x/Soul Capitol Limited Copy@1x.png"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/img/theme/profile.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/img/theme/landing.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/img/theme/promo-1.png"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "d0d448ee8af84ee003c0d613e28fa537",
+    "url": "/img/brand/pat-spots-006.png"
+  },
+  {
+    "revision": "3eacd63796de4b39bc102dae7b143ca5",
+    "url": "/JetBrainsMono-Regular.woff2"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "/img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "e13704b72eab1d462fc6cd3352fad42f",
+    "url": "/klein-vyper_optimized.svg"
+  },
+  {
+    "revision": "e518d69aeb0e270df2ba3e7583f0bac2",
+    "url": "/qrcode-solid.svg"
+  },
+  {
+    "revision": "248b9d625f793717be5b69e4971f8b3e",
+    "url": "/sync-solid.svg"
+  },
+  {
+    "revision": "66afe55f7908def509038b644b737bd0",
+    "url": "/logo_ren_beta_optimized.svg"
+  },
+  {
+    "revision": "017cf317057eaa6d9465496734782a31",
+    "url": "/newfavicon.svg"
+  },
+  {
+    "revision": "78e3bfd191f532cf904510327ca44e07",
+    "url": "/tokens/_ydai.png"
+  },
+  {
+    "revision": "0f2e130e8d70d5b5495617a83dfcd33d",
+    "url": "/tokens/_ytusd.png"
+  },
+  {
+    "revision": "a1d8eb953ddb1ce942acfb58df1e9f69",
+    "url": "/tokens/_ybusd.png"
+  },
+  {
+    "revision": "62be285d781cffe27f927e7f2b6a38e6",
+    "url": "/tokens/_yusdt.png"
+  },
+  {
+    "revision": "7a717f82fe9b2660e8a5e6b915281bfd",
+    "url": "/tokens/_ysusd.png"
+  },
+  {
+    "revision": "146ea02a5689840a92dfbac2df7d46b7",
+    "url": "/logo_optimized.svg"
+  },
+  {
+    "revision": "802b73d65fd8904431f08fcf257ecf28",
+    "url": "/tokens/_yusdc.png"
+  },
+  {
+    "revision": "3d19cddfc28bd2d6529f903cd09d52b7",
+    "url": "/tokens/ytusd.png"
+  },
+  {
+    "revision": "e157bcf70d04cd68d709b2622d53bbd7",
+    "url": "/tokens/btc.svg"
+  },
+  {
+    "revision": "f824cedc7b08e0e02f6a43e0a4fc0087",
+    "url": "/tokens/ysusd.png"
+  },
+  {
+    "revision": "bdb6ff351c908e4fd36b943a73f86f90",
+    "url": "/tokens/yusdc.png"
+  },
+  {
+    "revision": "d9791e92432144f062dfb58c9e0ecd40",
+    "url": "/tokens/yusdt.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/img/theme/team-2-800x800.jpg"
+  },
+  {
+    "revision": "74a20639087b68b82f222b1e26af2413",
+    "url": "/klein-vyper.svg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "ce8e2a4156448bf45ac70338ba0093f0",
+    "url": "/trash-alt-solid.svg"
+  },
+  {
+    "revision": "7f13d7e9d8723c2f964daed03e797eb7",
+    "url": "/img/brand/bg-mv-saturated.jpg"
+  },
+  {
+    "revision": "6bd2f2aae21a0eb1b9337124a53523fd",
+    "url": "/favicon-24x24.svg"
+  },
+  {
+    "revision": "ca3e0b726f8f9120179240eb61adefe6",
+    "url": "/img/homepage/bg-abstract-spirally-expanding-dust.png"
+  }
+];
